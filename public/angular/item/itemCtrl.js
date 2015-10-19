958b468d4a5c12da0040feb88caedafba1ce44db
Lovisa Nilsson <lo_nilsson@hotmail.com>
2015-10-19 13:11:50 +0200

    outfitCtrl

--- public/angular/item/itemCtrl.js
+++ public/angular/item/itemCtrl.js
@@ -1,10 +1,45 @@
 angular.module('item', [])
 
+.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
 
-.controller('CreateItemCtrl', ['$scope', 'Items', 'Users', '$location', function ($scope, Items, Users, $location) {
-  $scope.items = Items.query();
-  var user_id = "561cd088b360d5b022f3fcbe";
-  $scope.user = Users.get({id: user_id});
+  .when('/item', {
+      templateUrl: 'angular/item/addItem.html',
+      controller: 'CreateItemCtrl',
+      resolve: {
+        getUser: ['$route', 'Users', function($route, Users) {
+          var id = "561cd088b360d5b022f3fcbe";
+          return Users.get({id: id});
+        }],
+        itemsData: ['Items', function(Items) {
+          return Items.query();
+        }]
+      }
+   })
+
+  .when('/item/:id', {
+    templateUrl: 'angular/item/showItem.html',
+    controller: 'UpdateItemCtrl',
+    resolve: {
+      getUser: ['$route', 'Users', function($route, Users) {
+        var id = "561cd088b360d5b022f3fcbe";
+        return Users.get({id: id});
+      }],
+      itemsData: ['Items', function(Items) {
+
+        return Items.query();
+      }],
+      getItem: ['$route', 'Items', function($route, Items) {
+        var id = $route.current.params.id;
+        return Items.get({id: id});
+      }]    
+    }
+   });   
+}])
+
+.controller('CreateItemCtrl', ['$scope', 'Items', 'itemsData', 'getUser', '$location', function ($scope, Items, itemsData, getUser, $location) {
+  $scope.items = itemsData;
+  $scope.user = getUser;
 
   $scope.save = function(){
     if(!$scope.img || $scope.img.length < 1) return;
@@ -12,7 +47,7 @@ angular.module('item', [])
       	img: $scope.img,
       	tags: [],
       	types: [],
-      	owner: user_id});
+      	owner: $scope.user._id});
 
       item.$save(function(){
       	$scope.items.push(item);
@@ -21,26 +56,12 @@ angular.module('item', [])
   }
 }])
 
-.controller('UpdateItemCtrl', ['$scope', '$routeParams', 'Users', 'Items', '$location', function ($scope, $routeParams, Users, Items, $location) {
-	$scope.items = Items.query();
-	var user_id = "561cd088b360d5b022f3fcbe";
-	$scope.user = Users.get({id: user_id});
-	$scope.item = Items.get({id: $routeParams.id});
-
-
-	// $scope.inTypesArray = function(string){
-
-	// 	var arr = $scope.item.types;
-		
-	// 	if (arr.indexOf(string) == -1){
-	// 		return false	
-	// 	} else {
-	// 		return true;
-	// 	}
-	// }
+.controller('UpdateItemCtrl', ['$scope', 'Items', '$routeParams', 'getUser', 'itemsData', '$location', 'getItem', function ($scope, Items, $routeParams, getUser, itemsData, $location, getItem) {
+	$scope.items = itemsData;
+	$scope.user = getUser;
+  $scope.item = getItem;
 
 	$scope.inArray = function(where, string){
-
 		var arr = $scope.item[where];
 		
 		if (arr.indexOf(string) == -1){
@@ -51,36 +72,18 @@ angular.module('item', [])
 	}
 
 	$scope.push = function(where, string){
-	  	$scope.item[where].push(string);
-  	}
+  	$scope.item[where].push(string);
+    console.log($scope.item);
+	}
 
-  	$scope.splice = function(where, string){
-  		var arr = $scope.item[where];
-  		var index = arr.indexOf(string);
-  		$scope.item[where].splice(index, 1);
-  	}
+	$scope.splice = function(where, string){
+		var arr = $scope.item[where];
+		var index = arr.indexOf(string);
+		$scope.item[where].splice(index, 1);
+	}
 
-  	$scope.save = function(){
-  		Items.update({id: $scope.item._id}, $scope.item);
-      	$location.path('/item');
-  	}
-
-}])
-
-
-
-
-
-.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-
-  .when('/item', {
-      templateUrl: 'angular/item/addItem.html',
-      controller: 'CreateItemCtrl',
-   })
-
-    .when('/item/:id', {
-      templateUrl: 'angular/item/showItem.html',
-      controller: 'UpdateItemCtrl'
-   });   
+	$scope.save = function(){
+		Items.update({id: $scope.item._id}, $scope.item);
+    	$location.path('/item');
+	}
 }]);
--- public/angular/outfit/outfitCtrl.js
+++ public/angular/outfit/outfitCtrl.js
@@ -1,29 +1,54 @@
 angular.module('outfit', [])
 
+.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+   .when('/outfit', {
+      templateUrl: 'angular/outfit/addOutfit.html',
+      controller: 'CreateOutfitCtrl',
+    })
 
-.controller('CreateOutfitCtrl', ['$scope', 'Users', 'Items', 'Outfits', '$location', function ($scope, Users, Items, Outfits, $location) {
-	$scope.items = Items.query();
-  	var user_id = "561cd088b360d5b022f3fcbe";
-  	$scope.user = Users.get({id: user_id});
-  	$scope.outfit = Outfits.query();
+    .when('/outfit/:id', {
+      templateUrl: 'angular/outfit/showOutfit.html',
+      controller: 'UpdateOutfitCtrl',
+      resolve: {
+        getUser: ['$route', 'Users', function($route, Users) {
+          var userid = "561cd088b360d5b022f3fcbe";
+          return Users.get({id: userid});
+        }],
+        itemsData: ['Items', function(Items) {
+          return Items.query();
+        }],
+        getOutfit: ['$route', 'Outfits', function($route, Outfits) {
+          var id = $route.current.params.id;
+          return Outfits.get({id: id});
+        }]
+      }
+   });
+}])
+
+.controller('CreateOutfitCtrl', ['$scope', 'Users', 'Items', 'Outfits', '$location', 
+  function ($scope, Users, Items, Outfits, $location) {
+  	var userid = "561cd088b360d5b022f3fcbe";
+    $scope.user = Users.get({id: userid});
+    $scope.items = Items.query();
+  	$scope.outfits = Outfits.query();
   	$scope.blocks = [];
 
-  	if(!$scope.coll) {
-  		$scope.coll = [];
-  	}
+    if(!$scope.coll) {
+      $scope.coll = [];
+    }
+
+    if(!$scope.clicked) {
+      $scope.clicked = [];
+    }
    	
   	$scope.newBlock = function(){
   		var number = $scope.blocks.length+1;
   		$scope.blocks.push(number);
   	}
 
-  	addToCollection = function(arr, number){
-  		$scope.coll[number] = arr;
-  		console.log($scope.coll);
-  	}
-
   	$scope.findItems = function(){
-		$scope.arr = [];
+		  $scope.arr = [];
 	  	var option = $scope.option;
 	  	var items = $scope.items;
 
@@ -31,34 +56,77 @@ angular.module('outfit', [])
 	  		if(items[i].types.indexOf(option) > -1){
 	  			$scope.arr.push(items[i]._id);
 	  		}
-		}
+  		}
 
-		addToCollection($scope.arr, $scope.block);
+  		$scope.coll[$scope.block] = $scope.arr;
   	}
 
-  	$scope.random = function(){
-  		var coll = $scope.coll;
+  	$scope.randomize = function(){
   		$scope.random = [];
+      $scope.outfitItems = [];
 
-		for (var i = 0, len = coll.length; i < len; i++) {
-	  		$scope.random.push(coll[i][Math.floor(Math.random() * coll[i].length)]);
-		}
+  		for (var i = 1, len = $scope.coll.length; i < len; i++) {
+          var blockArr = $scope.coll[i];
+          var randItem = blockArr[Math.floor(Math.random() * blockArr.length)];
+  	  		$scope.random.push(randItem);
+  		}
 
-		return $scope.random;
+      for (var i = 0, len = $scope.random.length; i < len; i++) {
+          var item = Items.get({id: $scope.random[i]});
+          $scope.outfitItems.push(item);
+      }
+      
   	}
+
+    $scope.clicking = function(index){
+      var item = $scope.outfitItems[index];
+      
+      if($scope.clicked.indexOf(item._id) == -1){
+        $scope.clicked.push(item._id);
+      } else {
+        var index = $scope.clicked.indexOf(item._id);
+        $scope.clicked.splice(index, 1);
+      }
+
+      console.log($scope.clicked);
+    }
+
+    // $scope.missMatch = function(){
+    //   for (var i = 0, len = $scope.random.length; i < len; i++) {
+    //     var randomId = $scope.random[i];
+    //     console.log('randomId:'+randomId);        
+    //     item = Items.get({id: randomId});
+    //     console.log('item:');
+    //     console.log(item);
+
+    //     for (var j = 0, len = $scope.clicked.length; j < len; j++) {
+    //     var clickedId = $scope.clicked[j];
+    //     console.log('clickedId: '+clickedId);
+
+    //       if(randomId != clickedId){
+    //         console.log('push');
+    //         item.missmatch.push(clickedId);
+    //         console.log('pushed');
+    //       }
+    //     }
+    //     console.log('varv');
+
+    //     Items.update({id: randomId}, item);      
+    //   }      
+    // }
+
+    $scope.saveOutfit = function(){
+      var outfit = new Outfits({items: $scope.random, owner: $scope.user._id});
+
+      outfit.$save(function(){
+        $scope.outfits.push(outfit);
+        $scope.outfitItems = [];
+      });
+    }
 
 }])
 
-
-.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-   .when('/outfit', {
-      templateUrl: 'angular/outfit/addOutfit.html',
-      controller: 'CreateOutfitCtrl'
-    })
-
-    .when('/outfit/:id', {
-      templateUrl: 'angular/outfit/showOutfit.html',
-      controller: 'UpdateOutfitCtrl'
-   });
+.controller('UpdateOutfitCtrl', ['$scope', 'Users', 'Items', 'Outfits', 'getOutfit', '$location', 
+  function ($scope, Users, Items, Outfits, getOutfit, $location) {
+    $scope.outfit = getOutfit;
 }]);
